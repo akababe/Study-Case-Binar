d329e021cb5c14708f3a8aa4217d54467f86ea18
akaarinda <akaarinda@gmail.com>
2022-02-22 00:38:27 +0700

    change database con

--- config/config.js
+++ config/config.js
@@ -1,14 +1,18 @@
 // DATABASE_URL=postgres://postgres:akaarinda@localhost:5432/dbtest
 
-const Pool = require('pg').Pool
+const { Pool, Client } = require('pg')
+const connectionString = process.env.DATABASE_URL
 const pool = new Pool({
-  user: process.env.USER_ID,
-  host: process.env.DB_HOST,
-  database: process.env.DB,
-  password: process.env.PASS_ID,
-  port: 5432,
+  connectionString,
 })
 
+// const pool = new Pool({
+//   user: process.env.USER_ID,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB,
+//   password: process.env.PASS_ID,
+//   port: 5432,
+// })
 
 module.exports = pool
 
