e8ca0182f4882563c5201d3a4b2db69063c9bb2d
akaarinda <akaarinda@gmail.com>
2022-02-22 00:04:19 +0700

    tril login api

--- router/login.js
+++ router/login.js
@@ -2,6 +2,13 @@ const router = require("express").Router();
 const login = require("../controller/login");
 // const restrict = require('../middlewares/restrict')
 
+const logGet = (req,res) => {
+    res.json({
+        msg :"Api Register sudah ada"
+    })
+}
+
+router.get("/login", logGet)
 router.post("/login", login.login);
 
 module.exports = router;
