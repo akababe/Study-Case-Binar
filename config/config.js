c22e75c5c9501577aa31a1df7e0380a29a962ce3
akaarinda <akaarinda@gmail.com>
2022-02-22 00:44:17 +0700

    change database con

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 // DATABASE_URL=postgres://postgres:akaarinda@localhost:5432/dbtest
 
 const { Pool, Client } = require('pg')
-const connectionString = process.env.DATABASE_URL
+const connectionString = 'postgres://kzgpsofluyrshb:b4892d115936049241a6b1469b480e42ca20c3a98ea05df96ac5fb87a370ec52@ec2-54-158-26-89.compute-1.amazonaws.com:5432/d3d5a9s5dc1l76'
 const pool = new Pool({
   connectionString,
 })
