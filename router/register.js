1a590d0c8f8436c83014361e74ac1c5e3e1a6c9a
akaarinda <akaarinda@gmail.com>
2022-02-22 00:00:14 +0700

    commit adding rout

--- router/register.js
+++ router/register.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 const register = require("../controller/register");
 
+const regGet = (req,res) => {
+    res.json({
+        msg :"Api Register sudah ada"
+    })
+}
 
+router.get("/register", regGet)
 router.post("/register", register.register)
+
 
 module.exports = router;
